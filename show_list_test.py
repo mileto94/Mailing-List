0a297645f427dba822ea9d876261cf4adb9adcff
kakato10 <svetimir.ignatov@gmail.com>
2014-03-22 14:20:50 +0200

    Forgot to add show_list_test.py file to the previous commit

--- show_list_test.py
+++ show_list_test.py
@@ -6,16 +6,24 @@ class TestShowListContent(unittest.TestCase):
     """docstring for TestShowListContent"""
 
     def setUp(self):
-        self.file_to_read = "FMI"
-
+        self.file_to_read = "FMI.txt"
 
     def test_show_list_content(self):
         file_open = open(self.file_to_read, "r")
         content = file_open.read()
-        file_open.seek(0) #returns the pointer at the begging of the file
-        new_list = mail.Mail_list({"lili":"lili@gmail.com"})
+        file_open.seek(0)
+        new_list = mail.Mail_list({"lili": "lili@gmail.com"})
+        names_and_mails1 = content.split('\n')
+        del names_and_mails1[len(names_and_mails1) - 1]
+        names_and_mails2 = []
+        for item in names_and_mails1:
+            names_and_mails2.append(item.split(' - '))
+        content = {}
+        for couple in names_and_mails2:
+            content[couple[0]] = couple[1]
         self.assertEqual(content, new_list.show_list_content(file_open))
         file_open.close()
+
 
 if __name__ == '__main__':
     unittest.main()
