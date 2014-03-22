16ed893c609e9a41cb8b22effcf70db3b15ef3e1
milka <mileto94@abv.bg>
2014-03-22 12:46:59 +0200

    merge files - function and test

--- merge_files_test.py
+++ merge_files_test.py
@@ -1,5 +1,6 @@
-import mail
 import unittest
+from merge_files import merge_files
+
 
 class TestMergingFiles(unittest.TestCase):
     """docstring for TestMergingFiles"""
@@ -8,7 +9,7 @@ class TestMergingFiles(unittest.TestCase):
         file_read1 = open(file_to_read1, "r")
         content1 = file_read1.read()
         file_read1.seek(0)
-        file_to_write = "merge"
+        file_to_write = "merge.txt"
         file_write = open(file_to_write, "w")
         file_to_read2 = "Hack Bulgaria.txt"
         file_read2 = open(file_to_read2, "r")
@@ -16,8 +17,8 @@ class TestMergingFiles(unittest.TestCase):
         file_read2.seek(0)
         data = content1 + content2
         file_write.write(data)
-
-        self.assertEqual(file_read1.read() + file_read2.read(), mail.merge_files("FMI.txt", "Hack Bulgaria.txt").show_list_content)
+        print(merge_files("FMI.txt", "Hack Bulgaria.txt", file_to_write))
+        self.assertEqual(file_read1.read() + file_read2.read(), merge_files("FMI.txt", "Hack Bulgaria.txt", file_to_write))
         file_read1.close()
         file_read2.close()
         file_write.close()
@@ -25,5 +26,3 @@ class TestMergingFiles(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
