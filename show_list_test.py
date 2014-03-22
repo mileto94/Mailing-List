377405fdc04b9af7a49511e7e8ee49b9f376519f
milka <mileto94@abv.bg>
2014-03-22 14:25:40 +0200

    ----

--- show_list_test.py
+++ show_list_test.py
@@ -6,14 +6,13 @@ class TestShowListContent(unittest.TestCase):
     """docstring for TestShowListContent"""
 
     def setUp(self):
-        self.file_to_read = "FMI"
-
+        self.file_to_read = "FMI.txt"
 
     def test_show_list_content(self):
         file_open = open(self.file_to_read, "r")
         content = file_open.read()
-        file_open.seek(0) #returns the pointer at the begging of the file
-        new_list = mail.Mail_list({"lili":"lili@gmail.com"})
+        file_open.seek(0)  #returns the pointer at the begging of the file
+        new_list = mail.Mail_list({"lili": "lili@gmail.com"})
         self.assertEqual(content, new_list.show_list_content(file_open))
         file_open.close()
 
